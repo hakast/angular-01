a7c3198a62e9882b851cc1d1f5ab09f5f1e2b6e4
hakast <hakan.astrom@student.kyh.se>
2019-01-09 13:51:44 +0100

    Last commit before start image gallery

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,11 +5,13 @@ import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
 import { ReadMoreServiceComponent } from './readMore/read-more-service/read-more-service.component';
+import { PicsComponent } from './pics/pics.component'
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'about', component: AboutComponent },
   { path: 'contact', component: ContactComponent },
+  { path: 'pics', component: PicsComponent },
   { path: 'readMoreService', component: ReadMoreServiceComponent },
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
 import { HomeComponent } from './home/home.component';
 import { ReadMoreServiceComponent } from './readMore/read-more-service/read-more-service.component';
-
 import { NavComponent } from './nav/nav.component';
-import { ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { PicsComponent } from './pics/pics.component';
 
 @NgModule({
   declarations: [
@@ -19,7 +19,8 @@ import { HttpClientModule } from '@angular/common/http';
     ContactComponent,
     HomeComponent,
     NavComponent,
-    ReadMoreServiceComponent
+    ReadMoreServiceComponent,
+    PicsComponent
   ],
   imports: [
     BrowserModule,
