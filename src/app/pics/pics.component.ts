8b6f32103d076ecdba3f31105ef755a26fe3b624
hakast <hakan.astrom@student.kyh.se>
2019-01-11 00:24:23 +0100

    Gallery working with thumnails and fullsize. New branch with more typescrpt code

--- src/app/pics/pics.component.ts
+++ src/app/pics/pics.component.ts
@@ -6,15 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pics.component.scss']
 })
 export class PicsComponent implements OnInit {
+  test () {
+    console.log('skit')
+      // <img [src]="../../assets/gallery/bild_1.jpg">
+  }
 
   constructor() { }
-
-  // tiles = [
-  //   { text: 'One', cols: 2, rows: 1, color: '#142A5C' },
-  //   { text: 'Two', cols: 1, rows: 1, color: '#B7A0E8' },
-  //   { text: 'Three', cols: 1, rows: 2, color: '#FF0000' },
-  //   { text: 'Four', cols: 3, rows: 1, color: '#D9EDD9' },
-  // ];
 
   ngOnInit() {
   }
