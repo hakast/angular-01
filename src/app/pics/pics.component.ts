10a9e1d69424868b90d7198cf5d9a42f86e86415
hakast <hakan.astrom@student.kyh.se>
2019-01-14 14:39:23 +0100

    Changed to dynamic generated Github  pics and accounts

--- src/app/pics/pics.component.ts
+++ src/app/pics/pics.component.ts
@@ -1,19 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
 
 @Component({
   selector: 'app-pics',
   templateUrl: './pics.component.html',
   styleUrls: ['./pics.component.scss']
 })
-export class PicsComponent implements OnInit {
-  test () {
-    console.log('skit')
-      // <img [src]="../../assets/gallery/bild_1.jpg">
-  }
 
-  constructor() { }
+
+export class PicsComponent implements OnInit {
+  users: Object;
+
+  constructor(private data: DataService) { 
+
+  }
 
   ngOnInit() {
-  }
+    this.data.getImages().subscribe(data => {
+      this.users = data
+      console.log(this.users);
+    }
+    );
 
+  }
 }
